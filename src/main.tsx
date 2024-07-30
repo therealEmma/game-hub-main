7673be4ee80a95842e6b960ead3008bc92abe15c
therealEmma <eozegbe68@gmail.com>
2024-07-30 13:41:02 +0100

    SetUp Routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
 import theme from "./theme";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
+        <RouterProvider router={router} />
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, Router } from "react-router-dom";
+import Layout from "./Pages/Layout";
+import HomePage from "./Pages/HomePage";
+import GameDetailPage from "./Pages/GameDetailPage";
+
+const router =  createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children:[
+            {index:true, element:<HomePage />},
+            {path:'games/:id', element:<GameDetailPage />}
+        ]
+    }
+]);
+export default router;
